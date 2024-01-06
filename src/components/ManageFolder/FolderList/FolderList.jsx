52daaed7f730658d0aee2161a462c63e4bed4150
annurtasnimislam <annurtasnimislam@gmail.com>
2024-01-06 22:56:47 +0600

    feat: sorting done

--- src/components/ManageFolder/FolderList/FolderList.jsx
+++ src/components/ManageFolder/FolderList/FolderList.jsx
@@ -2,16 +2,32 @@ import Folder from "../Folder/Folder";
 import classes from "./FolderList.module.css";
 
 export default function FolderList({ showArray, onFolderClick, deleteClick }) {
+  let order = true;
+
   return (
     <div className={classes.list}>
-      {showArray.map((folder, i) => (
-        <Folder
-          key={i}
-          folder={folder}
-          onFolderClick={onFolderClick}
-          deleteClick={deleteClick}
-        />
-      ))}
+      {showArray
+        .sort((a, b) =>
+          order
+            ? a.name > b.name
+              ? 1
+              : b.name > a.name
+              ? -1
+              : 0
+            : a.name < b.name
+            ? 1
+            : b.name < a.name
+            ? -1
+            : 0
+        )
+        .map((folder, i) => (
+          <Folder
+            key={i}
+            folder={folder}
+            onFolderClick={onFolderClick}
+            deleteClick={deleteClick}
+          />
+        ))}
     </div>
   );
 }
